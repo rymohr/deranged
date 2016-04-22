2bb4115e50f6da41a68762c7b9ec410a13011717
Ryan Mohr <ryan.mohr@gmail.com>
2016-04-22 11:53:38 -1000

    es5 rewrite

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
+'use strict';
+
 // For now we only support integer ranges
 //
 // TODO: add some tests
 //
 // deranged.parse('2005..2008,2010,2015..2018') => [2005, 2006, 2007, 2008, 2010, 2015, 2016, 2017, 2018]
 // deranged.stringify([2005, 2006, 2007, 2008, 2010, 2015, 2016, 2017, 2018]) => '2005..2008,2010,2015..2018'
-let RANGE = /^\d+[.]{2}\d+$/;
+var RANGE = /^\d+[.]{2}\d+$/;
 
-export function parse(raw, options = {numeric: true}) {
-  let values = [];
-  let tokens = raw.split(',');
+function parse(raw) {
+  var values = [];
+  var tokens = raw.split(',');
 
-  tokens.forEach((token) => {
-
+  tokens.forEach(function(token) {
     if (RANGE.test(token)) {
-      let [ rangeBegin, rangeEnd ] = token.split('..');
+      var match = token.split('..');
+      var rangeBegin = match[0];
+      var rangeEnd = match[1];
 
       for (var value = rangeBegin; value <= rangeEnd; value++) {
-        values.push(parseValue(value, options));
+        values.push(Number(value));
       }
     } else {
-      values.push(parseValue(token, options))
+      values.push(Number(token))
     }
   })
 
   return values;
 }
 
-export function stringify(values) {
-  let tokens = [];
-  let rangeBegin = null;
+function stringify(values) {
+  var tokens = [];
+  var rangeBegin = null;
 
   for (var i = 0; i < values.length; i++) {
-    let value = values[i];
-    let nextValue = values[i + 1];
+    var value = values[i];
+    var nextValue = values[i + 1];
 
     if (nextValue === value + 1) {
       if (rangeBegin === null) rangeBegin = value;
@@ -40,7 +43,7 @@ export function stringify(values) {
       if (rangeBegin === null) {
         tokens.push(value);
       } else {
-        tokens.push(`${rangeBegin}..${value}`);
+        tokens.push(rangeBegin + '..' + value);
         rangeBegin = null;
       }
     }
@@ -49,8 +52,4 @@ export function stringify(values) {
   return tokens.join(',');
 }
 
-function parseValue(value, options) {
-  return options && options.numeric ? Number(value) : value;
-}
-
-export default { parse, stringify };
+module.exports = { parse, stringify };
